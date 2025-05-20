02f34216876c2064fde6e5e60fde3b9b6a729aad
Diego Apugliese <dapugliese@gmail.com>
2025-05-20 20:43:28 -0300

    version variables

--- codigo.js
+++ codigo.js
@@ -1,6 +1,37 @@
- var v1 = document .getElementById("variable");
-  var nombre = "edwin"
-  var apellido = "machuca chavez"
-  var resultado1 = nombre + ' ' + apellido 
+ var v1 = document .getElementById("var_Texto");
+ var v2 = document .getElementById("var_Entera");
+ var v3 = document .getElementById("var_Booleana");
 
- v1.innerText +=  ' ' + resultado1  ;
+
+// Tipo String (Texto) 
+ var nombre = "edwin";
+
+// Tipo Number (Numerico) 
+ var edad = 50.2;
+
+// Tipo  Boolean (Booleano)
+ var vive = true;
+
+
+// Tipo Array (Arreglo - Vector)
+
+var vector = ['juan','pedro','manuel',true,13.37]
+
+//Operaciones
+var opNombre = 'Colegio' + ' ET Nro 12'
+var opEdad = 12.2 + 12.3
+var opVive = false + false;
+
+
+
+
+// Muestro las variables en los respectivos elementos html
+ v1.innerText +=  ' ' + opNombre  ;
+ v2.innerText +=  ' ' + opEdad  ;
+ //v3.innerText +=  ' ' + vector  ;
+
+ 
+ for (var i=0; i<=4 ; i++){
+
+    v3.innerText += '[' + vector[i] + ']' + ', ';
+ }
